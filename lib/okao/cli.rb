5703a80e0bcc9ffb14f36a8098188e3a49375372
ta-chibana <takuma.chibana314@gmail.com>
2016-09-20 21:20:22 +0900

    Change variable name

--- lib/okao/cli.rb
+++ lib/okao/cli.rb
@@ -5,26 +5,26 @@ module Okao
   class CLI < Thor
     OKAOS = ['(^-^)', '(´O`)', '(;-;)', '(´Д`)', '(｡-_-｡)', '(´･ω･`)', '(-_-;)', '(_ _)']
 
-    desc 'to_okao [text][--file_path file_path]', 'Convert plane text to okao text'
+    desc 'to_okao [text][--file_path file_path]', 'Convert normal text to okao text'
     option :file_path
     def to_okao(text = nil)
       okao_init
-      plane_text = load_file(options[:file_path])
-      plane_text ||= text
+      normal_text = load_file(options[:file_path])
+      normal_text ||= text
 
-      okao_text = Okao::Parser.to_okao(plane_text)
+      okao_text = Okao::Parser.to_okao(normal_text)
       puts okao_text
     end
 
-    desc 'to_text [okao][--file_path file_path]', 'Convert okao text to plane text'
+    desc 'to_text [okao][--file_path file_path]', 'Convert okao text to normal text'
     option :file_path
     def to_text(okao = nil)
       okao_init
       okao_text = load_file(options[:file_path])
       okao_text ||= okao
 
-      plane_text = Okao::Parser.to_text(okao_text)
-      puts plane_text
+      normal_text = Okao::Parser.to_text(okao_text)
+      puts normal_text
     end
 
     desc 'exec [okao][--file_path file_path]', 'Execute okao script'
